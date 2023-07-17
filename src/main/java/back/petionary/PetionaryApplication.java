332ebd9dc702296a7f4456791257c259b570eb7d
Hwae woon Jung <hoewoon@HoeStoryui-MacBookAir.local>
2023-07-17 23:43:54 +0900

    프로젝트 생성

---
+++ src/main/java/back/petionary/PetionaryApplication.java
@@ -0,0 +1,13 @@
+package back.petionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetionaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/back/petionary/PetionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package back.petionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
