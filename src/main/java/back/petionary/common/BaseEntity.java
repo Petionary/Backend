fb2a2a1267fdae3fa0c13cd1b6329686cf7be415
cousim46 <wjdghldns24@naver.com>
2023-07-23 02:13:23 +0900

    null 처리에 대한 메서드 추가ˆ

--- src/main/java/back/petionary/common/BaseEntity.java
+++ src/main/java/back/petionary/common/BaseEntity.java
@@ -1,7 +1,9 @@
 package back.petionary.common;
 
-import java.time.LocalDate;
+import back.petionary.common.exception.CustomErrorCode;
+import back.petionary.common.exception.CustomException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
@@ -21,12 +23,18 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
     @CreatedDate
     @Column(updatable = false, nullable = false)
     protected LocalDateTime createdAt;
+
     @LastModifiedBy
     @Column(nullable = true)
     protected LocalDateTime updatedAt;
+
+    protected  <T> T nullValidate(T element) {
+        return Optional.ofNullable(element).orElseThrow(()-> new CustomException(CustomErrorCode.NOT_VALUE));
+    }
 
 
 }
