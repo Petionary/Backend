6c32357fafbf647c61c8bac534432d554ac31b68
cousim46 <wjdghldns24@naver.com>
2023-07-23 02:32:25 +0900

    테스트 코드 추가 - 이메일, 핸드폰번호, 주소, 이름, 생년월일이 null도 들어왔을때에 대한 테스트코드

--- src/test/java/back/petionary/account/entity/AccountTest.java
+++ src/test/java/back/petionary/account/entity/AccountTest.java
@@ -37,7 +37,7 @@ class AccountTest {
     }
 
     @Test
-    @DisplayName("유저의 정보 중 빈값이 존재하면 예외가 발생한다.")
+    @DisplayName("유저의 정보 중 이메일이 null일 경우 예외가 발생한다.")
     void test2() {
         //given
         String email = null;
@@ -50,12 +50,69 @@ class AccountTest {
 
         //when && then
         assertThrows(CustomException.class,() -> new Account(email, phone, name, birth, address));
+    }
+    @Test
+    @DisplayName("유저의 정보 중 이름이 null일 경우 예외가 발생한다.")
+    void test3() {
+        //given
+        String email = "test@test.com";
+        String name = null;
+        LocalDate birth = LocalDate.of(1998,10,15);
+        String zipcode = "1111";
+        String detail = "상세주소";
+        String phone = "01011111111";
+        Address address = new Address(zipcode, detail);
 
+        //when && then
+        assertThrows(CustomException.class,() -> new Account(email, phone, name, birth, address));
+    }
+    @Test
+    @DisplayName("유저의 정보 중 생년월일이 null일 경우 예외가 발생한다.")
+    void test4() {
+        //given
+        String email = "test@test.com";
+        String name = "test";
+        LocalDate birth = null;
+        String zipcode = "1111";
+        String detail = "상세주소";
+        String phone = "01011111111";
+        Address address = new Address(zipcode, detail);
+
+        //when && then
+        assertThrows(CustomException.class,() -> new Account(email, phone, name, birth, address));
     }
 
     @Test
-    @DisplayName("주소 중 하나라도 빈 값이 존재하면 예외가 발생한다.")
-    void test3() {
+    @DisplayName("유저의 정보 중 핸드폰번호가 null일 경우 예외가 발생한다.")
+    void test5() {
+        //given
+        String email = "test@test.com";
+        String name = "테스트";
+        LocalDate birth = LocalDate.of(1998,10,15);
+        String zipcode = "1111";
+        String detail = "상세주소";
+        String phone = null;
+        Address address = new Address(zipcode, detail);
+
+        //when && then
+        assertThrows(CustomException.class,() -> new Account(email, phone, name, birth, address));
+    }
+
+    @Test
+    @DisplayName("유저의 정보 중 상세주소가 null일 경우 예외가 발생한다.")
+    void test6() {
+        //given
+        String zipcode = "1111";
+        String detail = null;
+
+        //when && then
+        assertThrows(CustomException.class,() -> new Address(zipcode, detail));
+    }
+
+
+    @Test
+    @DisplayName("주유저의 정보 중 우편번호가 존재하면 예외가 발생한다.")
+    void test7() {
         //given
         String zipcode = null;
         String detail = "상세주소";
