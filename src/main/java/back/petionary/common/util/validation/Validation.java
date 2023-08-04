714395c87e08f32c2d85266328da86c6fc1bcb6f
cousim46 <wjdghldns24@naver.com>
2023-08-05 00:55:56 +0900

    리뷰 반영 - 제약 조건 체크하는 방식 수정

--- src/main/java/back/petionary/common/util/validation/Validation.java
+++ src/main/java/back/petionary/common/util/validation/Validation.java
@@ -2,22 +2,24 @@ package back.petionary.common.util.validation;
 
 import back.petionary.common.exception.type.CommonErrorCode;
 import back.petionary.common.exception.CommonException;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Validation {
-    public static <T> T validateNull(T element) {
-        if(element == null) {
-            throw new CommonException(CommonErrorCode.NOT_VALUE);
+
+    public static <T> void validateNull(T... elements) {
+        for (T element : elements) {
+            if(element == null) {
+                throw new CommonException(CommonErrorCode.NOT_VALUE);
+            }
         }
-        return element;
     }
 
-    public static <T> T validateSize(T element, int size) {
-        if(element instanceof String) {
-            if(element.toString().length() > size) {
+    public static <T> void validateSize(T element, int size) {
+        if (element instanceof String) {
+            if (element.toString().length() > size) {
                 throw new CommonException(CommonErrorCode.EXCEED_SIZE);
             }
-            return element;
         }
-        return null;
     }
 }
