53c2eeba7064e1037bc7c785fa65c9e22dfeb5d6
cousim46 <wjdghldns24@naver.com>
2023-09-24 23:01:21 +0900

    젠킨스 연동 테스트

--- src/main/java/back/petionary/PetionaryApplication.java
+++ src/main/java/back/petionary/PetionaryApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class PetionaryApplication {
 
     public static void main(String[] args) {
-        System.out.println("젠킨스 테스트중입니다ㅁ.");
+        System.out.println("젠킨스 테스트중입니다2.");
         SpringApplication.run(PetionaryApplication.class, args);
     }
 
