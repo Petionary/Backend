20e1332cf5531aff1ac4b486a1d8ead1602e4754
lby9906 <lby9906@naver.com>
2024-02-04 19:46:58 +0900

    오류 수정 후 자주 가는 지역, 닉네임 api

--- src/main/java/back/petionary/application/address/controller/AddressController.java
+++ src/main/java/back/petionary/application/address/controller/AddressController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("address")
+@RequestMapping("/address")
 public class AddressController {
     private final AddressService addressService;
 
