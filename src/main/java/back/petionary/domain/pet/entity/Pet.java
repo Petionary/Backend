e9cf48399c29f90a6c8ed59d9f4fb9d351d43604
lby9906 <lby9906@naver.com>
2024-03-02 20:18:24 +0900

    mypet 페이지 코드 리뷰 재반영

--- src/main/java/back/petionary/domain/pet/entity/Pet.java
+++ src/main/java/back/petionary/domain/pet/entity/Pet.java
@@ -4,6 +4,7 @@ import back.petionary.application.mypet.enums.PetGender;
 import back.petionary.application.mypet.enums.PetSpecies;
 import back.petionary.common.BaseEntity;
 import back.petionary.domain.account.entity.Account;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 @Entity
+@AllArgsConstructor
 public class Pet extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Account account;
@@ -33,17 +35,6 @@ public class Pet extends BaseEntity {
 
     private String content;
 
-    public Pet(Account account, String imgUrl, String petName, LocalDate petBirth, PetGender petGender, PetSpecies petSpecies, String speciesDetail, String content) {
-        this.account = account;
-        this.imgUrl = imgUrl;
-        this.petName = petName;
-        this.petBirth = petBirth;
-        this.petGender = petGender;
-        this.petSpecies = petSpecies;
-        this.speciesDetail = speciesDetail;
-        this.content = content;
-    }
-
     public void update(String imgUrl, String petName, LocalDate petBirth, PetGender petGender, PetSpecies petSpecies, String speciesDetail, String content) {
         this.imgUrl = imgUrl;
         this.petName = petName;
@@ -53,7 +44,4 @@ public class Pet extends BaseEntity {
         this.speciesDetail = speciesDetail;
         this.content = content;
     }
-
-
-
 }
