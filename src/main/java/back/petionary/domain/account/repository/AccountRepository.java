93b9c95a0c3702dfb9fc2ab57ea984b78ea853a3
jhw1015 <jhw1015@bank-mall.co.kr>
2024-03-02 20:04:24 +0900

    fix : 불필요한 코드 삭제

--- src/main/java/back/petionary/domain/account/repository/AccountRepository.java
+++ src/main/java/back/petionary/domain/account/repository/AccountRepository.java
@@ -12,6 +12,4 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByEmail(String email);
 
     Optional<Account> findByEmail(String email);
-
-    Optional<Account> findById(Long Id);
 }
