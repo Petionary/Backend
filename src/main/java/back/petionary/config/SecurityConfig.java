4ec3ad14d31622827e0f9ee4109d954b5ef490e6
jhw1015 <jhw1015@bank-mall.co.kr>
2024-03-10 21:20:49 +0900

    feat : cors 설정

--- src/main/java/back/petionary/config/SecurityConfig.java
+++ src/main/java/back/petionary/config/SecurityConfig.java
@@ -1,24 +1,37 @@
 package back.petionary.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConifg = new CorsConfiguration();
+        corsConifg.setAllowedOrigins(List.of("http://localhost:3000/"));
+        corsConifg.addAllowedHeader("*");
+        corsConifg.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConifg);
+        return source;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
-            .cors().disable()
+            .cors().configurationSource(corsConfigurationSource()).and()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-
             .exceptionHandling()
             .and()
             .authorizeRequests().antMatchers("/**", "/login/**").permitAll()
