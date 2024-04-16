3921c6ba8ed24dba4fa8bf5b78ffe0f7d2962c5d
cousim46 <wjdghldns24@naver.com>
2024-04-16 18:10:21 +0900

    tt

--- src/main/java/back/petionary/application/account/controller/LoginController.java
+++ src/main/java/back/petionary/application/account/controller/LoginController.java
@@ -19,7 +19,7 @@ public class LoginController {
     }
 
     @GetMapping("/login/oauth2/naver-login")
-    public LoginToken naverLogin(@RequestParam String code) {ㅁㅁa2
+    public LoginToken naverLogin(@RequestParam String code) {ㅁㅁa21
         return socialLoginService.getNaverAccessToken(code);
     }
 }
